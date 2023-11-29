84147740ed76cf039bda7cf3d17f8f89aee78f06
BADREDDINEKHELEF <152400283+BADREDDINEKHELEF@users.noreply.github.com>
2023-11-29 20:14:48 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Creating the window with all its awesome snaky features
+		Menu snake = new Menu();
+		snake.initComponents();
+		new Menu().setVisible(true);
+
+	}
+}
